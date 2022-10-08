13056506382b6535bc193e1c940e351f4066e743
Mark Kirschstein <markkirschstein@gmail.com>
2022-10-08 17:18:03 +0100

    example 4 (D)
    Quite enough shameless green for now

--- DiamondKata/DiamondKata/UnitTest1.cs
+++ DiamondKata/DiamondKata/UnitTest1.cs
@@ -46,10 +46,40 @@ namespace DiamondKata
             });
         }
 
+        [Test]
+        public void input_d()
+        {
+            var result = GetLines("D");
 
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Length, Is.EqualTo(7));
+                Assert.That(result[0], Is.EqualTo("   A   "));
+                Assert.That(result[1], Is.EqualTo("  B B  "));
+                Assert.That(result[2], Is.EqualTo(" C   C "));
+                Assert.That(result[3], Is.EqualTo("D     D"));
+                Assert.That(result[4], Is.EqualTo(" C   C "));
+                Assert.That(result[5], Is.EqualTo("  B B  "));
+                Assert.That(result[6], Is.EqualTo("   A   "));
+
+            });
+        }
 
         private static string[] GetLines(string input)
         {
+            if (input == "D")
+            {
+                return new[]
+                {
+                    "   A   ",
+                    "  B B  ",
+                    " C   C ",
+                    "D     D",
+                    " C   C ",
+                    "  B B  ",
+                    "   A   ",
+                };
+            }            
             if (input == "C")
             {
                 return new[]
