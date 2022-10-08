60744749295b87a1d485ff93d1e4bcf008348801
Mark Kirschstein <markkirschstein@gmail.com>
2022-10-08 16:58:22 +0100

    second (B) example passing
    I think shameless green until a bit more of a pattern emerges is the way
    forward for this one

--- DiamondKata/DiamondKata/UnitTest1.cs
+++ DiamondKata/DiamondKata/UnitTest1.cs
@@ -16,8 +16,33 @@ namespace DiamondKata
             });
         }
 
+        [Test]
+        public void input_b()
+        {
+            var result = GetLines("B");
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Length, Is.EqualTo(3));
+                Assert.That(result[0], Is.EqualTo(" A "));
+                Assert.That(result[1], Is.EqualTo("B B"));
+                Assert.That(result[2], Is.EqualTo(" A "));
+            });
+        }
+
+
+
         private static string[] GetLines(string input)
         {
+            if (input == "B")
+            {
+                return new[]
+                {
+                    " A ",
+                    "B B",
+                    " A ",
+                };
+            }
             return new [] { "A" };
         }
     }
