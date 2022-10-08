f3248c6a977f4937d25f8caf646b41fca77241fb
Mark Kirschstein <markkirschstein@gmail.com>
2022-10-08 17:02:32 +0100

    example 3 (C) passing
    Have noticed that the number of lines in the result array is the same as
    the length of each line
    
    Have noticed when copying the old examples to modify into new ones, the
    steps are
    
    1) pad each existing line with whitespace on left/right
    2) duplicate the existing middle example
    3) insert the new character
    
    A appears to be a special case - it only has a single char in its line,
    others have two.
    
    Think I'll do D as shameless then start de-duping, as that'll give me
    three examples of the same thing (A not being 'the same')

--- DiamondKata/DiamondKata/UnitTest1.cs
+++ DiamondKata/DiamondKata/UnitTest1.cs
@@ -30,10 +30,37 @@ namespace DiamondKata
             });
         }
 
+        [Test]
+        public void input_c()
+        {
+            var result = GetLines("C");
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result.Length, Is.EqualTo(5));
+                Assert.That(result[0], Is.EqualTo("  A  "));
+                Assert.That(result[1], Is.EqualTo(" B B "));
+                Assert.That(result[2], Is.EqualTo("C   C"));
+                Assert.That(result[3], Is.EqualTo(" B B "));
+                Assert.That(result[4], Is.EqualTo("  A  "));
+            });
+        }
+
 
 
         private static string[] GetLines(string input)
         {
+            if (input == "C")
+            {
+                return new[]
+                {
+                    "  A  ",
+                    " B B ",
+                    "C   C",
+                    " B B ",
+                    "  A  ",
+                };
+            }            
             if (input == "B")
             {
                 return new[]
