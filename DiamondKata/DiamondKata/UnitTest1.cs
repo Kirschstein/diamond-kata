9474200c6f5366f78d7ca95a970cd1ab8497178c
Mark Kirschstein <markkirschstein@gmail.com>
2022-10-09 20:50:50 +0100

    refactor: compute constants - pattern now more clear

--- DiamondKata/DiamondKata/UnitTest1.cs
+++ DiamondKata/DiamondKata/UnitTest1.cs
@@ -71,33 +71,33 @@ namespace DiamondKata
             {
                 return new[]
                 {
-                    Pad("A", 4 - 1),
-                    Pad("B B", 5 - 3),
-                    Pad("C   C", 6 - 5),
-                    Pad("D     D", 7 - 7),
-                    Pad("C   C", 6 - 5),
-                    Pad("B B", 5 - 3),
-                    Pad("A", 4 - 1),
+                    Pad("A", 3),
+                    Pad("B B", 2),
+                    Pad("C   C", 1),
+                    Pad("D     D", 0),
+                    Pad("C   C", 1),
+                    Pad("B B", 2),
+                    Pad("A", 3),
                 };
             }            
             if (input == "C")
             {
                 return new[]
                 {
-                    Pad("A", 3 - 1),
-                    Pad("B B", 4 - 3),
-                    Pad("C   C", 5 - 5),
-                    Pad("B B", 4 - 3),
-                    Pad("A", 3 - 1),
+                    Pad("A", 2),
+                    Pad("B B", 1),
+                    Pad("C   C", 0),
+                    Pad("B B", 1),
+                    Pad("A", 2),
                 };
             }            
             if (input == "B")
             {
                 return new[]
                 {
-                    Pad("A", 2 - 1),
-                    Pad("B B", 3 - 3),
-                    Pad("A", 2 - 1),
+                    Pad("A", 1),
+                    Pad("B B", 0),
+                    Pad("A", 1),
                 };
             }
             return new [] { "A" };
